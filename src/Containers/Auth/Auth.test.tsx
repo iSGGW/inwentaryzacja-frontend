a973412610c90a79d6724f68b55ccaed468a1ea2
Zuzanna Dudzinska <zuzannadudzinska96@gmail.com>
2022-03-28 20:05:37 +0200

    test

--- src/Containers/Auth/Auth.test.tsx
+++ src/Containers/Auth/Auth.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Auth from "./Auth";
 
-test("renders learn react link", () => {
+test("renders learn react lin", () => {
   render(<Auth />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
