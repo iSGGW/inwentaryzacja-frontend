11252ef39341258bc974f6096322c4280a293359
unknown <marcinluksz.kielbik@gmail.com>
2022-04-02 22:22:57 +0200

    loginpanel

--- src/Containers/Auth/Auth.tsx
+++ src/Containers/Auth/Auth.tsx
@@ -2,7 +2,29 @@ import React from "react";
 import "./Auth.css";
 
 function Auth() {
-  return <div className="App">lorem ipsum</div>;
+  return <div className="App">
+    
+    <header  className="App-header">
+    <h1>Zaloguj się</h1>
+    <form>
+        <label>
+          <p>Nazwa użytkownika</p>
+          <input type="text"/>
+        </label>
+        <label>
+          <p>Hasło</p>
+          <input type="password"/>
+        </label>
+        <div>
+          <button type="submit">Potwierdź</button>
+        </div>
+        <div>
+          <button>Utwórz konto</button>
+        </div>
+    </form>
+
+    </header>
+  </div>;
 }
 
 export default Auth;
