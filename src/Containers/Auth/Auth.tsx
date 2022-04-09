199cf820e2b02356501938303705e1314cac4ec0
Marcin Kiełbik <marcinlukasz.kielbik@gmail.com>
2022-04-09 17:30:53 +0200

    Update Login panel

--- src/Containers/Auth/Auth.tsx
+++ src/Containers/Auth/Auth.tsx
@@ -1,29 +1,28 @@
 import "./Auth.css";
 
 function Auth() {
-  return <div className="App">
-    
-    <header  className="App-header">
-    <h1>Zaloguj się</h1>
-    <form>
-        <label>
-          <p>Nazwa użytkownika</p>
-          <input type="text"/>
-        </label>
-        <label>
-          <p>Hasło</p>
-          <input type="password"/>
-        </label>
-        <div>
-          <button type="submit">Potwierdź</button>
-        </div>
-        <div>
-          <button>Utwórz konto</button>
-        </div>
-    </form>
-
-    </header>
-  </div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Logowanie</h1>
+        <form>
+          <label>
+            <p>Nazwa użytkownika:</p>
+            <input type="text" />
+          </label>
+          <label>
+            <p>Hasło:</p>
+            <input type="password" />
+          </label>
+          <div>
+            <button id="btn-submit" type="submit">
+              Potwierdź
+            </button>
+          </div>
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default Auth;
