97f36242b984c6d0f06c49b0a0f39b06f1b03ef5
DamianBaryga <damian.baryga@sofomo.com>
2022-06-17 19:40:45 +0200

    Create Status component

--- src/Components/Description/Description.tsx
+++ src/Components/Description/Description.tsx
@@ -1,15 +1,9 @@
 import type { FunctionComponent } from "react";
-import { Badge, Descriptions, Empty } from "antd";
-import type { PresetStatusColorType } from "antd/lib/_util/colors";
+import { Descriptions, Empty } from "antd";
 import type { searchResult } from "src/App/Entities";
+import Status from "src/Components/Status/Status";
 
 import styles from "./Description.module.css";
-
-interface Status {
-  id: searchResult["status"];
-  name: searchResult["name"];
-  badge: PresetStatusColorType;
-}
 
 interface DescriptionProps {
   result?: searchResult;
@@ -18,42 +12,6 @@ interface DescriptionProps {
 export const Description: FunctionComponent<DescriptionProps> = ({
   result,
 }) => {
-  const statuses: Status[] = [
-    {
-      id: "1",
-      name: "Na stanie",
-      badge: "success",
-    },
-    {
-      id: "2",
-      name: "Brak",
-      badge: "error",
-    },
-    {
-      id: "3",
-      name: "Do utylizacji",
-      badge: "warning",
-    },
-    {
-      id: "4",
-      name: "Zutylizowane",
-      badge: "default",
-    },
-  ];
-
-  const getBadge = () => {
-    if (result) {
-      const matchingStatus = statuses.find(
-        (status) => status.id === result.status
-      );
-      if (matchingStatus) {
-        return (
-          <Badge status={matchingStatus.badge} text={matchingStatus.name} />
-        );
-      }
-    }
-  };
-
   return (
     <div className={styles.description}>
       <h3>Szczegóły</h3>
@@ -69,7 +27,9 @@ export const Description: FunctionComponent<DescriptionProps> = ({
         <Descriptions bordered layout={"vertical"}>
           <Descriptions.Item label="ID">{result.id}</Descriptions.Item>
           <Descriptions.Item label="Nazwa">{result.name}</Descriptions.Item>
-          <Descriptions.Item label="Status">{getBadge()}</Descriptions.Item>
+          <Descriptions.Item label="Status">
+            <Status itemStatus={result.status} />
+          </Descriptions.Item>
           <Descriptions.Item label="Piętro">
             {result.floor.name}
           </Descriptions.Item>
---
+++ src/Components/Status/Status.tsx
@@ -0,0 +1,57 @@
+import type { FunctionComponent } from "react";
+import { Badge } from "antd";
+import type { searchResult } from "src/App/Entities";
+import type { PresetStatusColorType } from "antd/lib/_util/colors";
+
+interface Status {
+  id: searchResult["status"];
+  name: searchResult["name"];
+  badge: PresetStatusColorType;
+}
+
+interface StatusProps {
+  itemStatus: searchResult["status"];
+}
+
+const Status: FunctionComponent<StatusProps> = ({ itemStatus }) => {
+  const statuses: Status[] = [
+    {
+      id: "1",
+      name: "Na stanie",
+      badge: "success",
+    },
+    {
+      id: "2",
+      name: "Brak",
+      badge: "error",
+    },
+    {
+      id: "3",
+      name: "Do utylizacji",
+      badge: "warning",
+    },
+    {
+      id: "4",
+      name: "Zutylizowane",
+      badge: "default",
+    },
+  ];
+
+  const getBadge = () => {
+    if (itemStatus) {
+      const matchingStatus = statuses.find(
+        (status) => status.id === itemStatus
+      );
+      if (matchingStatus) {
+        return (
+          <Badge status={matchingStatus.badge} text={matchingStatus.name} />
+        );
+      }
+    }
+    return null;
+  };
+
+  return getBadge();
+};
+
+export default Status;
