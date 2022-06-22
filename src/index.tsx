7952d531eaf34877b3ef5076e5abe1ed1e9ae70f
DamianBaryga <damian.baryga@sofomo.com>
2022-06-22 20:38:52 +0200

    Add and redirect to the server's url path

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,12 @@ import ReactDOM from "react-dom";
 import App from "./App/App";
 import { BrowserRouter as Router } from "react-router-dom";
 
+if (window.location.href === `${window.location.origin}/`) {
+  window.location.href = "/inz/build";
+}
+
 ReactDOM.render(
-  <Router>
+  <Router basename={"inz/build"}>
     <App />
   </Router>,
   document.getElementById("root")
