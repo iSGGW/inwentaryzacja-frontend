1c3285f48a0b5c0037a9d0802cbff4ecd3d8aa79
Zuzanna Dudzinska <zuzannadudzinska96@gmail.com>
2022-06-26 10:29:15 +0200

    resolve conflicts

--- src/Components/Status/Status.tsx
+++ src/Components/Status/Status.tsx
@@ -39,7 +39,9 @@ const Status: FunctionComponent<StatusProps> = ({ itemStatus }) => {
 
   const getBadge = () => {
     if (itemStatus) {
-      const matchingStatus = statuses.find((status) => status.id == itemStatus);
+      const matchingStatus = statuses.find(
+        (status) => status.id == itemStatus
+      );
       if (matchingStatus) {
         return (
           <Badge status={matchingStatus.badge} text={matchingStatus.name} />
